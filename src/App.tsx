0d576f94efb8f869fa9e3f418d8d64af457b1589
DominicS99 <dominic.sagen@gmail.com>
2025-04-01 19:48:13 -0700

    Added player buttons

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import './App.css';
 const MOVES = ["rock", "paper", "scissors"] as const;
 type Move = (typeof MOVES)[number];
 
-function getRandomMove() : Move {
+function getRandomMove(): Move {
   const idx = Math.floor(Math.random() * MOVES.length);
   return MOVES[idx];
 }
 
 function App() {
-  const [computerMove, _setComputerMove] = React.useState<Move>(getRandomMove());
+  const [computerMove, _setComputerMove] = React.useState(getRandomMove());
+  const [playerMove, setPlayerMove] = React.useState<Move | null>(null);
 
   const containerStyle = {
     display: "flex",
@@ -21,8 +22,25 @@ function App() {
   return (
     <div style={containerStyle}>
       <span>Computer Move: {computerMove}</span>
+      <span>Player Move: {playerMove}</span>
+      <button onClick={() => setPlayerMove("rock")}>Rock</button>
+      <button onClick={() => setPlayerMove("paper")}>Paper</button>
+      <button onClick={() => setPlayerMove("scissors")}>Scissors</button>
     </div>
   );
 }
 
 export default App;
+
+
+function id<T>(x: T): T {
+  return x;
+}
+
+function stringify<T>(x: T): string {
+  return String(x);
+}
+
+function returnSecond<T, U>(x: T, y: U): U[] {
+  return [y, y, y];
+}
